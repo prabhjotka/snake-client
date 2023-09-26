4385853b70cf9a2c82949bab8a62761eabe57146
Prabh <sonukaur68@gmail.com>
2023-09-26 09:55:40 -0600

    refactor play.js

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
+const clientS = require("./client");
 
-// establishes a connection with the game server
-const connect = function() {
-    const conn = net.createConnection({
-        host: "165.227.47.243",
-        port: "50541"
-        ,
-    });
 
-    // interpret incoming data as text
-    conn.setEncoding("utf8");
-    conn.on("data", (meassge) => {
-        console.log(meassge);
-    });
-    return conn;
-};
 
-console.log("Connecting ...");
-connect();
+
